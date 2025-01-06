6e0119da9a6b7738d2bab6d1a51b3f5b85e15bcb
ragu6936 <bhj1684@naver.com>
2025-01-06 18:35:46 +0900

    Spring Boot 수정 - Value 어노테이션 추가

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -3,17 +3,18 @@ package com.example.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  @Value("${clientOrigin}")
+
+  @Value("${CLIENT_ORIGIN}")
   private String CLIENT_ORIGIN;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:5173")
-        .allowedOrigins(CLIENT_ORIGIN)
+				.allowedOrigins("http://localhost:5173", CLIENT_ORIGIN) 
 				.allowedMethods("GET", "POST")
 				.allowedHeaders("*")
 				.allowCredentials(true);
