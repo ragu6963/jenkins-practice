a90c78302bb07145d076f29cae7eb671763ff8b9
ragu6936 <bhj1684@naver.com>
2025-01-06 18:12:13 +0900

    Spring Boot WebConfig 수정 - 환경 변수 사용 코드

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -6,12 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Value("{CLIENT_ORIGIN}")
+  String CLIENT_ORIGIN;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOrigins("http://localhost:5173")
-        .allowedOrigins(${CLIENT_ORIGIN})
+        .allowedOrigins(CLIENT_ORIGIN)
 				.allowedMethods("GET", "POST")
 				.allowedHeaders("*")
 				.allowCredentials(true);
